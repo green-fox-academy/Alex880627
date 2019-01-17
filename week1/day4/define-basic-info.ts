63299e186522c8a38a3f2cf6c9068f537799e9f7
Illés Sándor <illes.alexander88@gmail.com>
2019-01-17 15:44:13 +0100

    Just finnished my personal data variables

---
+++ week1/day4/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName:string = 'Sanyi';
+let myAge:number = 30;
+let myHeight:number = 1.8;
+let myStatus:boolean = false;
+
+
+console.log('My name is '+myName);
+console.log('My age is '+myAge);
+console.log('My height is '+myHeight);
+console.log('Am I married? The Answer is: '+myStatus);
