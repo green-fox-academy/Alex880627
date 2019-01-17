5d8a861ad91a0485fccbd317fb2433e680f26dd3
Illés Sándor <illes.alexander88@gmail.com>
2019-01-17 15:24:12 +0100

    I swapped this two files, look at it please

---
+++ week1/day4/swap.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let num1:number = 77;
+let num2:number = 45;
+let temp:number = 0;
+
+temp = num1;
+num1 = num2;
+num2 = temp;
+
+console.log('Num1 = '+num1);
+console.log('Num2 = '+num2);
+
