de8043bceca0a6da3e3b583b5881941088c0d40e
Illés Sándor <illes.alexander88@gmail.com>
2019-01-17 14:46:55 +0100

    Finnished the two numbers exercise

---
+++ week1/day4/two-numbers.ts
@@ -0,0 +1,13 @@
+'use strict';
+let num1 = 13;
+let num2 = 22;
+
+function littleMath() {
+    console.log(num1 + num2);
+    console.log(num2 - num1);
+    console.log(num1 * num2);
+    console.log(num2 / num1);
+    console.log(num2 % num1);
+}
+
+littleMath();
