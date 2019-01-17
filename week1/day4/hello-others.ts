181c4076337e9b14b2922b43f91fe8b005b56ef8
Illés Sándor <illes.alexander88@gmail.com>
2019-01-17 14:43:11 +0100

    Finnsihed with last two

---
+++ week1/day4/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let name1:string = "Esther";
+
+let name2:string = "Mary";
+
+let name3:string = "Joe";
+
+function nameFunction() { console.log('Hello, ' + name1); console.log('Hello, ' + name2); console.log('Hello, ' + name3); }
+
+nameFunction();
---
+++ week1/day4/introduce-yourself.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+let myName:string = 'Sanyi';
+
+let age:number = 30;
+
+let height:number = 180;
+
+function details() {
+    console.log(myName);
+    console.log(age);
+    console.log(height);
+}
+details();
