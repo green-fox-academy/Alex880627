e67af69c8a73088373bb8c4a27b6e9937d202eff
Illés Sándor <illes.alexander88@gmail.com>
2019-01-17 16:11:44 +0100

    The Math stuff done

---
+++ week1/day4/variable-mutation.ts
@@ -0,0 +1,79 @@
+'use strict';
+
+let a: number = 3;
+a = a+10;
+// make it bigger by 10
+
+console.log(a);
+
+let b: number = 100;
+// make it smaller by 7
+b = b-7;
+console.log(b);
+
+let c: number = 44;
+// double c's value
+c = c*2;
+console.log(c);
+
+let d: number = 125;
+// divide d's value by 5
+d = d/5;
+console.log(d);
+
+let e: number = 8;
+// what's the cube of e's value?
+e*=e
+console.log(e);
+
+let f1: number = 123;
+let f2: number = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+if (f1 > f2 ){
+    console.log('F1 is bigger than F2')
+}
+else{
+    console.log('F2 is bigger than F1')
+}
+
+let g1: number = 350;
+let g2: number = 200;
+// tell if the double of g2 is bigger than g1 (pras a boolean)
+if (g1 > g2*2 ){
+    console.log('g1 is bigger than g2')
+}
+else{
+    console.log('g2 is bigger than g1')
+}
+
+let h: number = 1357988018575474;
+// tell if 11 is a divisor of h (as a boolean)
+
+if (h%11===0){
+    console.log(h+' is a divisor of 11');
+} else {
+    console.log(h+' is not a divisor of 11');
+}
+
+let i1: number = 10;
+let i2: number = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+
+if (i1>i2*i2 && i1 < i2*i2*i2){
+    console.log('Its true');
+} else {
+    console.log('Its not true');
+}
+
+let j: number = 1521;
+// tell if j is divisible by 3 or 5 (as a boolean)
+if (j%3 === 0 || j%5 === 0){
+    console.log('The statement is true');
+} else {
+    console.log('The statement is false');
+}
+
+let k: string = 'Apple';
+// fill the k variable with its content 4 times
+
+console.log(k+k+k+k);
