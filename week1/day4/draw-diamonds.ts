57b0688f04c24118e25420d99595bdbaf634fcf1
Illés Sándor <illes.alexander88@gmail.com>
2019-01-18 14:48:24 +0100

    Finnished with this pyramid, I almost shit cube

---
+++ week1/day4/draw-diamonds.ts
@@ -0,0 +1,68 @@
+'use strict';
+
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is
+
+// this counts the breakingpoint
+
+let lineCount: number = 9;
+let breakPoint: number;
+
+if (lineCount % 2 === 1) {
+    breakPoint = (lineCount - 1) / 2;
+}
+else { breakPoint = (lineCount / 2) }
+
+console.log(breakPoint);
+
+//It is checking the if the number is odd or even
+let oddOreven: number = 5;
+if (breakPoint % 2 === 1) {
+    oddOreven = 0;
+}
+else {
+    oddOreven = 1;
+}
+
+
+for (let i: number = 0; i <= breakPoint; i++) {
+    let row: string = '';
+
+    for (let j: number = 1; j <= (breakPoint - i); j++) {
+        row += ' ';
+    }
+
+    for (let k: number = 1; k <= (i * 2) + 1; k++) {
+        row += '*';
+
+    }
+
+    console.log(row);
+
+}
+
+for (let i: number = breakPoint; i >= 0; i--) {
+    let row2: string = '';
+
+    for (let k: number = 1; k <= (breakPoint - i) + oddOreven; k++) {
+        row2 += ' ';
+    }
+
+    for (let j: number = 1; j <= (i * 2) + 1 - (2*oddOreven); j++) {
+        row2 += '*';
+    }
+
+    console.log(row2);
+
+}
--- week1/day4/draw-pyramid.ts
+++ week1/day4/draw-pyramid.ts
@@ -11,24 +11,18 @@
 // The pyramid should have as many lines as lineCount is
 
 
-let lineCount: number = 4;
+let lineCount: number = 8;
 
 
-for (let i: number = 1; i <= lineCount; i++) {
+for (let i: number = 0; i <= lineCount; i++) {
     let row: string = '';
 
     for (let j: number = 1; j <= (lineCount - i); j++) {
         row += ' ';
     }
 
-    for (let k: number = 1; k <= i; k++) {
-        if (k === 1) {
+    for (let k: number = 1; k <= (i*2)+1; k++) {
             row += '*';
-        }
-        else {
-            row += '**';
-        }
-
 
     }
 
